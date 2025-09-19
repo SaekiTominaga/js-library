f9fba889595e19481795d66c12220f41b48a15ac
SaekiTominaga <kcsu3470+github@gmail.com>
2025-09-19 13:47:27 +0900

    sqlite-utility を作成

---
+++ packages/sqlite-utility/src/index.ts
@@ -0,0 +1,3 @@
+import { jsToSQLite, sqliteToJS, prepareSelect, prepareInsert, prepareUpdate, prepareDelete } from './sqlite.ts';
+
+export { jsToSQLite, sqliteToJS, prepareSelect, prepareInsert, prepareUpdate, prepareDelete };
