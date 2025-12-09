a763a414b522d18ef199ca55e976a338788511f5
SaekiTominaga <kcsu3470+github@gmail.com>
2025-12-09 16:43:05 +0900

    index.ts 修正

--- packages/sqlite-utility/src/index.ts
+++ packages/sqlite-utility/src/index.ts
@@ -1,12 +1,8 @@
-import { jsToSQLiteComparison, jsToSQLiteAssignment, sqliteToJS, prepareSelect, prepareInsert, prepareUpdate, prepareDelete } from './sqlite.ts';
+import { jsToSQLiteComparison, jsToSQLiteAssignment, sqliteToJS } from './sqlite.ts';
 
 export {
 	jsToSQLiteComparison,
 	jsToSQLiteAssignment,
 	jsToSQLiteAssignment as jsToSQLite, // Ensuring backward compatibility
 	sqliteToJS,
-	prepareSelect,
-	prepareInsert,
-	prepareUpdate,
-	prepareDelete,
 };
