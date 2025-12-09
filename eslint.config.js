155aed629501f623a7431ec7847c78f4500925f2
SaekiTominaga <kcsu3470+github@gmail.com>
2025-12-09 22:27:47 +0900

    不要ルール削除

--- eslint.config.js
+++ eslint.config.js
@@ -24,19 +24,6 @@ export default [
 		},
 	},
 	{
-		files: ['packages/console-locale-timestamp/src/Console.ts'],
-		rules: {
-			'class-methods-use-this': 'off',
-			'no-console': 'off',
-		},
-	},
-	{
-		files: ['packages/console-locale-timestamp/sample.ts'],
-		rules: {
-			'no-console': 'off',
-		},
-	},
-	{
 		files: ['packages/wareki/src/Wareki.ts'],
 		rules: {
 			'class-methods-use-this': 'off',
